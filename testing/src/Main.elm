931b6b3a215b1790ff832a239f47361e2a09806a
Rob <hello@robmcdonald.me>
2024-11-18 21:24:28 +0000

    Add some stuff
    - Forgot commas deconstructing
    - Add a form for easier string changing
    - Fix a couple things

--- testing/src/Main.elm
+++ testing/src/Main.elm
@@ -16,17 +16,25 @@ module Main exposing (main)
     Questions
     ---------
     1. Can we only use `Advert a` in the update function?
-        - Is the only way to narrow types for view with a type alias record?
+        - I think so! I think you need to first narrow type for `alias record`
+        - Then narrow type for the particular `alias record a` update type
     2. We're using a `visible` boolean for our `Meals` to filter on
         - Is there a better way to do this?
         - We need the "full list" and the "filtered list" somehow
+    3. Should I use alphabetical for all the things?
+        - `{ record deconstruct }`) etc
+        - `model.record`, `model.order` etc
 
     Wishlist
     --------
-    1. Build a basic user
-    2. Build a list of meals
-       - Mix of recipes and eating out
-    3. Filter by one ingredient
+    1. Currently filtering by a single ingredient ... which is a form
+    2. `List Ingredient` is in TWO places: `model.meals` and `model.advert`
+    3. What about errors? Currently does nothing if `"ingredient"` doesn't exist
+    4. Start thinking about better structures for:
+        - `Model` and `Currency` etc
+        - `User` and adding/editing (plus wishlist etc)
+        - Master list of `Ingredients`
+        - User interactions (three ingredients picked, filters, etc)
 -}
 
 import Debug
@@ -42,34 +50,86 @@ type EatType
   | Vegetarian
   | Vegan
 
+eatTypeString : EatType -> String
+eatTypeString eat =
+  case eat of
+    Meat ->
+      "Meat"
+    Vegetarian ->
+      "Vegetarian"
+    Vegan ->
+      "Vegan"
+
 type MealType
   = Recipe
   | Restaurant
   | Cafe
   | MealDeal
 
+mealTypeString : MealType -> String
+mealTypeString meal =
+  case meal of
+      Recipe ->
+        "Recipe"
+      Restaurant ->
+        "Restaurant"
+      Cafe ->
+        "Cafe"
+      MealDeal ->
+        "Meal Deal"
+
 type Currency
   = Gold
   | Silver
-  | Bronze
+  | Bronze String
 
-type Filter =
+currencyString : Currency -> String
+currencyString currency =
+  case currency of
+      Gold ->
+        "Gold: loved it"
+      Silver ->
+        "Silver: liked it"
+      Bronze str ->
+        "Bronze: " ++ str
+
+type alias Filter =
   String
 
-type Ingredient =
+type alias Ingredient =
   String
 
 type Ingredients =
   List Ingredient
 
+type alias Time =
+  (Int, Int)
+
+timeString : Time -> String
+timeString time =
+  "Time: "
+  ++ (String.fromInt Tuple.first time)
+  ++ ":"
+  ++ (String.fromInt Tuple.second time)
+
 type alias Meal =
-  { eatType : EatType
+  { visible : Bool
+  , eatType : EatType
   , mealType : MealType
+  , title : String
   , ingredients : Ingredients
-  , time : (Int, Int) -- This is going to get confusing: recipe? restaurant?
+  , time : Time -- This is going to get confusing: recipe? restaurant?
   }
 
-{- #! I think this can only be used for `Update` -}
+type alias Advert =
+  { visible : Bool -- Has it been filtered?
+  , url : String
+  , image : Int
+  , title : String
+  , info : String -- rich text
+  , ingredients : Ingredients -- Linked to `visible`
+  }
+
 type alias Visible a =
   { a
     | visible : Bool -- Filtered?
@@ -83,6 +143,8 @@ type alias Model =
   , location : String -- Radius
   , meals : List Meal
   , adverts : List Advert
+  , form : String
+  , error : String
   }
 
 baseUrl : String
@@ -97,8 +159,61 @@ userImage = baseUrl ++ "user.jpg"
 -- 2. We're filtering the advert and meal lists
 -- 3. Click a button to filter
 
-viewAdvert : Bool -> String -> Int -> String -> String -> List String -> Html msg
-viewAdvert visible url image title info ingredients =
+viewForm : String -> Html Msg
+viewForm string =
+  form [ onClick Save ]
+    [ input
+        [ type_ "text"
+        , value string
+        ]
+    , button
+        [ disabled (String.empty string) ]
+        [ text "Filter by ingredient" ]
+    ]
+
+
+viewUser : Model -> Html msg
+viewUser model =
+  article []
+    [ img [ src userImage ] []
+    , h2 [] [ text model.realName ]
+    , h3 [] [ text model.userName ]
+    , p [] [ text model.userPref ]
+    ]
+
+
+viewMeals : Meals -> Html msg
+viewMeals meals =
+  List.map viewMeal meals
+
+viewMeal : Meal -> Html msg
+viewMeal { visible, eatType, mealType, title, ingredients, time } =
+  if visible then
+    -- This is probably poor use of HTML5 tags
+    article []
+      [ small [] [ text (viewMealType eatType mealType) ]
+      , H2 [] [ text ]
+      , viewIngredients ingredients
+      , time
+      ]
+
+viewMealType : EatType -> MealType -> Html msg
+viewMealType eat meal =
+  String.concat
+    [ "Type: "
+    , (eatTypeString eat)
+    , ", "
+    , "Meal: "
+    , (mealTypeString meal)
+    ]
+
+
+viewAdvert : List Advert -> Html msg
+viewAdvert adverts =
+  List.map viewAdvert adverts
+
+viewAdvert : Advert -> Html msg
+viewAdvert { visible, url, image, title, info, ingredients } =
   if visible then
     -- This is probably poor use of HTML5 tags
     article []
@@ -110,27 +225,19 @@ viewAdvert visible url image title info ingredients =
   else
     text ""
 
+
 viewIngredients : Ingredients -> Html msg
 viewIngredients ingredients =
   List.map (\ingredient -> li [] [ text ingredient ]) ingredients
-
-viewUser : Model -> Html msg
-viewUser model =
-  article []
-    [ img [ src userImage ] []
-    , h2 [] [ text model.realName ]
-    , h3 [] [ text model.userName ]
-    , p [] [ text model.userPref ]
-    ]
 
 selectedIngredient : Maybe Ingredient -> Html msg
 selectedIngredient ingredient =
   case ingredient of
     Nothing ->
       p [] [ text "" ]
-    Just ingredient ->
+    Just filter ->
       p []
-        [ text (ingredient ++ " selected")
+        [ text (filter ++ " selected")
         , span [] [ button [ onClick Reset ] [ text "Reset" ] ]
         ]
 
@@ -140,12 +247,12 @@ view model =
     [ div [ class "grid" ]
       [div [] -- Main content
           [ selectedIngredient model.filter
+          , viewForm model.form
           , viewUser model -- !# Split out `model.user` as nested record?
-          , viewMeals meals
-          , button [ onClick (ClickedFilter "beef") ] [ text "Filter by ingredient" ]
+          , viewMeals model.meals
           ]
       , div [] -- Sidebar
-          [ viewAdvert adverts ]
+          [ viewAdvert model.adverts ]
       ]
     ]
 
@@ -156,6 +263,8 @@ view model =
 
 type Msg
   = ClickedFilter String
+  | Save
+  | Reset
 
 updateMeals : Filter -> List Meal -> List Meal
 updateMeals filter meals =
@@ -187,16 +296,17 @@ filterByIngredient filter ingredients =
 update : Msg -> Model -> Model
 update msg model =
   case msg of
-      ClickedFilter String ->
+      Save ->
         { model
-            | meals = updateMeals string model.meals
-            , adverts = updateAdverts string model.adverts
+            | meals = updateMeals model.form model.meals
+            , adverts = updateAdverts model.form model.adverts
         }
 
       Reset ->
         { model
             | meals = updateMeals "" model.meals
             , adverts = updateAdverts "" model.adverts
+            , form = ""
         }
 
 
